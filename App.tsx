128103e1501c51f311255d2a4c76fc574f84bbb4
Jeremy Pittard <jeremy@jpittard.net>
2024-07-28 14:56:04 +0800

    Move nav to own folder.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-// In App.js in a new project
-
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./src/screens/home";
-
-const Stack = createNativeStackNavigator();
+import AppNavigation from "./src/navigation/app-navigation/AppNavigation";
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigation />;
 }
 
 export default App;
